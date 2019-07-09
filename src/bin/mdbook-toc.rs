80faf141da2515eb098ce051eaa155b50c6b6afe
Jan-Erik Rediger <janerik@fnordig.de>
2019-07-09 11:15:10 +0200

    Update dependencies and refactor mdbook-toc into a preprocessor

---
+++ src/bin/mdbook-toc.rs
@@ -0,0 +1,64 @@
+extern crate clap;
+extern crate mdbook;
+extern crate mdbook_toc;
+extern crate serde_json;
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+use mdbook::errors::Error;
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor};
+use mdbook_toc::Toc;
+
+use std::io;
+use std::process;
+
+pub fn make_app() -> App<'static, 'static> {
+    App::new("mdbook-toc")
+        .about("mdbook preprocessor to Table of Contents")
+        .subcommand(
+            SubCommand::with_name("supports")
+                .arg(Arg::with_name("renderer").required(true))
+                .about("Check whether a renderer is supported by this preprocessor"))
+}
+
+fn main() {
+    let matches = make_app().get_matches();
+
+    if let Some(sub_args) = matches.subcommand_matches("supports") {
+        handle_supports(sub_args);
+    } else {
+        if let Err(e) = handle_preprocessing() {
+            eprintln!("{}", e);
+            process::exit(1);
+        }
+    }
+}
+
+fn handle_preprocessing() -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+    if ctx.mdbook_version != mdbook::MDBOOK_VERSION {
+        eprintln!(
+            "Warning: The mdbook-toc preprocessor was built against version \
+             {} of mdbook, but we're being called from version {}",
+            mdbook::MDBOOK_VERSION,
+            ctx.mdbook_version
+        );
+    }
+
+    let processed_book = Toc.run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed_book)?;
+
+    Ok(())
+}
+
+fn handle_supports(sub_args: &ArgMatches) -> ! {
+    let renderer = sub_args.value_of("renderer").expect("Required argument");
+    let supported = Toc.supports_renderer(&renderer);
+
+    // Signal whether the renderer is supported by exiting with 1 or 0.
+    if supported {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
