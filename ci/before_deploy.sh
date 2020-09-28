4577ca5510ea4d1e213e0aa17f6cadcef6984b6f
Jan-Erik Rediger <janerik@fnordig.de>
2020-09-28 17:47:23 +0200

    Don't compile with LTO

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin mdbook-toc --target $TARGET --release -- -C lto
+    cross rustc --bin mdbook-toc --target $TARGET --release
 
     cp target/$TARGET/release/mdbook-toc $stage/
 
